efb3294d38e3f97fd121ef17a6eb2c99bc27feb3
jjh68069 <jahi3695@colorado.edu>
2017-10-23 12:22:51 -0600

    Start working on Flask Login

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Repo for Redhat IOT Project Backend
 
 This project will live on the server as connected Docker containers. The three essential containers will be the webserver (Nagios or Apache2), the application (flask python app), and a database to persist the data to disk (MySQL).  
+
+To run the Flask app on the local net for dev work: flask run --host=0.0.0.0 --port=80
+
+
--- app/app.py
+++ app/app.py
@@ -1,1 +1,21 @@
-#Flask app to read in and store data from sensors
+from flask import Flask, render_template, url_for, redirect, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Try Again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+if __name__ == '__main__':
+    app.run()
